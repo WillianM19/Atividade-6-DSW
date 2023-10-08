ebaeb3998be80131ae1e13b6b36f86ac3f5bb150
WillianM19 <willianm1928@gmail.com>
2023-10-08 12:40:07 -0300

    feat: finalizando relacionamentos

--- discos/models.py
+++ discos/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.artist_name
+    
+class phonographicLabel(models.Model):
+    phonographicLabel_name = models.CharField(max_length=50, default="vazio")
+    
+    def __str__(self):
+        return self.phonographicLabel_name
+
 class disc(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=800)
-    phonographicLabel = models.CharField(max_length=200)
+    phonographicLabel = models.ForeignKey(phonographicLabel, on_delete=models.CASCADE)
     year = models.IntegerField()
     country = models.CharField(max_length=20)
     genre = models.CharField(max_length=20)
     amount = models.IntegerField()
+    artist = models.ManyToManyField(Artist)
+    
+    def __str__(self):
+        return self.name
+
+    
--- discos/views.py
+++ discos/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from . import models
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html', {'discs': models.disc.objects.all()})
